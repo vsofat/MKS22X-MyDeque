8472ee0f21cdbc03ea78a953497cf355c178c429
vsofat <vsofat00@stuy.edu>
2019-04-08 00:36:34 -0400

    calc

---
+++ Calculator.java
@@ -0,0 +1,23 @@
+public class Calculator{
+
+  public static double eval(String s){
+    //first, split the string into smaller strings
+    String[] split = s.split(" ");
+    MyDeque<Double> nums = new MyDeque<Double>();
+
+    double num = 0;
+    int i=0;
+
+    while (i < split.length) {
+
+      //System.out.println(nums);
+      try {
+        num = Double.parseDouble(split[i]);
+        nums.addLast(num);
+      }catch (NumberFormatException e) {
+        //reaches here if not a number.
+        String data = split[i];
+        if (cur.equals("+")) {
+          double curr_num = nums.getLast();
+          //need to remove right away so getLast() works next.
+          nums.removeLast();
